804ed61a97dc8009ab64a038b5897ef942476b80
Sudheer <sudheer@it-admin.local>
2021-03-22 16:27:25 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">Notes App</h1>
+        </header> 
+        <AmplifySignOut />
+      </div>
+     
+    )
+  }
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { notes:[] }
+  }
+
+  async componentDidMount(){
+  }  
+
+  render() {
+    return (
+      <AmplifyAuthenticator>
+      <div className="row">
+        <div className="col m-3">
+          <Header/>
+        </div> 
+      </div> 
+      </AmplifyAuthenticator>
+    );
+  }
 }
 
 export default App;
